be11147574fd96d5bcb2bca321b018e222ad43ce
zaza <contact@zaza.com>
2020-05-04 18:47:36 -0500

    mi primer commit en ruby

---
+++ primer_programa.rb
@@ -0,0 +1,22 @@
+=begin
+	Aquí hacemos la definición de la clase Hola mundo
+	Colocamos el método constructor
+	Y por último creamos un método saluda
+=end
+ 
+
+
+
+ class HolaMundo
+ 	def initialize()
+ 	end
+	def saluda()
+ 		puts "Hola mundo" #Primer comentario
+ 	end
+
+ end #fin de clase
+
+ #Creamos el objeto de la clase Hola Mundo
+ objeto = HolaMundo.new()
+ objeto.saluda
+ 
